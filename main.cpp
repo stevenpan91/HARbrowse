4cf607018b30b903303ade35c8634387d1f0e5f4
Steven Pan <stevenpan91@gmail.com>
2017-01-13 06:27:18 +0900

    First button

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <QApplication>
+#include <QFont>
+#include <QPushButton>
+
+int main (int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QPushButton quit("Quit");
+
+    quit.resize(100,30);
+    quit.setFont(QFont("Times",18,QFont::Bold));
+
+    QObject::connect(&quit, SIGNAL(clicked()), &app, SLOT(quit()));
+
+    quit.show();
+
+    return app.exec();
+
+
+}
